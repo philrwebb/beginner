47b55eeb1dd3accce45d59e0a96df82f7fd1ade9
philrwebb <prwebb@optusnet.com.au>
2021-09-18 13:55:06 +0800

    form padding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -252,6 +252,7 @@
                     class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   >
                     <option>United States</option>
+                    <option>Australia</option>
                     <option>Canada</option>
                     <option>Mexico</option>
                   </select>
