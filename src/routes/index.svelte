3d2566543edcc44bfa769bb6c9b3e3561ea422ba
philrwebb <prwebb@optusnet.com.au>
2021-09-18 12:20:16 +0800

    added tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the excellent documentation</p>
+<script>
+  export let url = "https://kit.svelte.dev";
+</script>
+
+<div class="text-center">
+  <h1 class="font-bold text-indigor-500">Welcome to SvelteKit</h1>
+  <button class="bg-indigo-500 hover:bg-indigo-700 text-white rounded px-4">Click</button>
+  <p>Visit <a href={url}>{url}</a> to read the excellent documentation</p>
+</div>
