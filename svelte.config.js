3a92afad9ae5f0af4fc32570daea3add15a7053d
philrwebb <prwebb@optusnet.com.au>
2021-09-18 09:34:57 +0800

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+const config = {
+	kit: {
+		adapter: adapter(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
