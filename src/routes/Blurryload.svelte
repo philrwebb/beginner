4ed99905e8885b51c2b5a236f254c5b79b4f9c5d
philrwebb <prwebb@optusnet.com.au>
2021-09-20 15:24:23 +0800

    daisyui

---
+++ src/routes/Blurryload.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+
+<section class="bg">
+    <div class="loading-text">0%</div>
+</section>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Roboto:wght@400;700&display=swap');
+
+    * {
+        box-sizing: border-box;
+    }
+
+    
+</style>
